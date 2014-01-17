49a24a43d5b21b6e8058fa07fe5645ebbb078336
Nik Voss <niklas.voss@gmail.com>
2014-01-17 14:13:27 +0000

    Command line options added and log function implemented.

--- src/main.cc
+++ src/main.cc
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <streambuf>
 #include <iostream>
+#include <sstream>
 
 #include <v8.h>
 #include <cppa/cppa.hpp>
@@ -11,14 +12,34 @@ using namespace v8;
 using namespace std;
 using namespace cppa;
 
+void log(const FunctionCallbackInfo<Value> &args) {
+    bool first = true;
+    for (int i = 0; i < args.Length(); i++) {
+        HandleScope handle_scope(args.GetIsolate());
+        if (first) {
+            first = false;
+        } else {
+            aout << " ";
+        }
+        String::Utf8Value utf8(args[i]);
+        aout << *utf8;
+    }
+    aout << endl;
+}
+
+Handle<Context> create_veta_context(Isolate* isolate) {
+    Handle<ObjectTemplate> global = ObjectTemplate::New();
+    global->Set(String::New("log"), FunctionTemplate::New(log));
+    return Context::New(isolate, NULL, global);
+}
+
 void veta_actor(string file) {
     auto isolate = Isolate::New();
     {
         Locker locker(isolate);
         Isolate::Scope isolate_scope(isolate);        
         HandleScope handle_scope(isolate);
-        //Handle<ObjectTemplate> object_template = Local<ObjectTemplate>::New(isolate, global);
-        Handle<Context> context = Context::New(isolate);
+        Handle<Context> context = create_veta_context(isolate);
         {
             Context::Scope context_scope(context);
     
@@ -42,7 +63,7 @@ void veta_actor(string file) {
 
             Handle<Value> result = script->Run();
 
-            if (!result.IsEmpty()) {
+            if (!result->IsUndefined()) {
                 String::Utf8Value utf8(result);
                 aout << *utf8 << endl;
             }
@@ -51,17 +72,34 @@ void veta_actor(string file) {
     isolate->Dispose();
 }
 
+int argv_to_int(char* arg) {
+    stringstream ss;
+    ss << arg;
+    int result;
+    ss >> result;
+    return result;
+}
+
 int main(int argc, char* argv[]) {
     std::string file_arg = "";
-    if (argc > 1) { 
-        file_arg = argv[1]; 
+    int num_actors = 1;
+    if (argc > 1) for (int i = 1; i < argc; i++) { 
+        if (strcmp(argv[i], "-n") == 0 && (i+1) < argc) {
+            i += 1;
+            num_actors = argv_to_int(argv[i]);            
+        } else {
+            string temp(argv[i]);
+            if (temp.size() > 3 && temp.substr(temp.size() - 3) == ".js") {
+                file_arg = temp;
+            }
+        }
     }
     if (file_arg == "") {
         return 0;
     }
     
     set_default_scheduler(4);
-    for(int i = 0; i < 100; i++) spawn(veta_actor, file_arg);
+    for(int i = 0; i < num_actors; i++) spawn(veta_actor, file_arg);
     await_all_others_done();
     shutdown(); 
 
