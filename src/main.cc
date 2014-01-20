50afa248eae269113396ee770664ab6a6a1cb83d
Nik Voss <niklas.voss@gmail.com>
2014-01-20 00:33:22 +0000

    Fixed problem but encountering next problem.

--- src/main.cc
+++ src/main.cc
@@ -31,68 +31,60 @@ void log(const FunctionCallbackInfo<Value> &args) {
     aout << endl;
 }
 
-map<string, shared_ptr<Extension>> extension_map;
-
-unique_ptr<ExtensionConfiguration> get_extension_configuration(const string &file) {
-    if (extension_map.find(file) == extension_map.end()) { // TODO: possible memory issue
-        ifstream file_stream(file.c_str());
-        if (!file_stream) {
-            aout << "Error: Unable to open file: " << file << endl;
-            return NULL;
-        }
-        string file_source;    
-        file_stream.seekg(0, std::ios::end);   
-        file_source.reserve(file_stream.tellg());
-        file_stream.seekg(0, std::ios::beg);
-        file_source.assign((std::istreambuf_iterator<char>(file_stream)), std::istreambuf_iterator<char>()); 
-        
-        shared_ptr<Extension> extension(new Extension(file.c_str(), file_source.c_str(), 0, 0, file_source.size()));
-        extension_map[file] = extension;       
-        RegisterExtension(extension.get()); 
-        aout << "Extension registered!" << endl;
-    }
-    // aout << v8::RegisteredExtension::first_extension()->extension->name();
-    const char* extensions[] = { file.c_str() };
-    //vector<char*> extension_vector;
-    //transform(begin(extensions), end(extensions), back_inserter(extension_vector), [](string& s){ s.push_back(0); return &s[0]; });
-    //extension_vector.push_back(nullptr);
-    return unique_ptr<ExtensionConfiguration>(new ExtensionConfiguration(1, extensions));
-}
+map<string, Extension*> extension_map;
 
 Handle<Context> create_veta_context(Isolate* isolate, const string &file) {
     Handle<ObjectTemplate> global = ObjectTemplate::New();
     global->Set(String::New("log"), FunctionTemplate::New(log));
-    return Context::New(isolate, get_extension_configuration(file).get(), global);
+    const char* extensions[] = { file.c_str() };
+    return Context::New(isolate, new ExtensionConfiguration(1, extensions), global);
 }
 
-void veta_actor(string file) {
+int preload_file(string file) { // , bool base_checked = false) {
+    if (file.substr(file.size() - 3) != ".js") {
+        file += ".js";
+    }
+    if (extension_map.find(file) == extension_map.end()) {
+        ifstream file_stream(file.c_str());
+        if (!file_stream) {
+            // if (!base_checked) {
+            //     file_stream = check_other_includes(file); 
+            //     if (!file_stream)
+            //         return -2;
+            //
+            // } else {
+            cout << "Error: Unable to open file: " << file << endl;
+            return -1;
+            // }           
+        }
+        string file_source;
+        file_stream.seekg(0, ios::end);   
+        file_source.reserve(file_stream.tellg());
+        file_stream.seekg(0, ios::beg);
+        file_source.assign((istreambuf_iterator<char>(file_stream)), istreambuf_iterator<char>());
+        // int include_error = check_source_includes(file_source);
+        // if (include_error != 0)
+        //     return include_error;
+        // NOTE: additional check in map possible because of includes
+        Extension* extension = new Extension(file.c_str(), file_source.c_str(), 0, 0, file_source.size());
+        extension_map[file] = extension;
+        RegisterExtension(extension); 
+        cout << "Extension registered!" << endl;
+    }
+    return 0;
+}
+
+void veta_actor(const string &file) {
     auto isolate = Isolate::New();
     {
         Locker locker(isolate);
         Isolate::Scope isolate_scope(isolate);        
         HandleScope handle_scope(isolate);
-        if (file.substr(file.size() - 3) != ".js") {
-            file += ".js";
-        }
         Handle<Context> context = create_veta_context(isolate, file);
         {
-            Context::Scope context_scope(context);
-    
-            //ifstream file_stream(file.c_str());
-            //if (!file_stream) {
-            //    aout << "Error: Unable to open file: " << file << endl;
-            //    return;
-            //}
-            //string file_source;    
-            //file_stream.seekg(0, std::ios::end);   
-            //file_source.reserve(file_stream.tellg());
-            //file_stream.seekg(0, std::ios::beg);
-            //file_source.assign((std::istreambuf_iterator<char>(file_stream)), std::istreambuf_iterator<char>()); 
-
-            //auto source = String::NewFromUtf8(isolate, file_source.c_str());
-            auto script = Script::Compile(String::NewFromUtf8(isolate, ""));
+            Context::Scope context_scope(context); 
+            auto script = Script::Compile(String::NewFromUtf8(isolate, "init();"));
             Handle<Value> result = script->Run();
-
             if (!result->IsUndefined()) {
                 String::Utf8Value utf8(result);
                 aout << *utf8 << endl;
@@ -127,7 +119,11 @@ int main(int argc, char* argv[]) {
     if (file_arg == "") {
         return 0;
     }
-    
+   
+    if (preload_file(file_arg) != 0) {
+        return 0;
+    }
+ 
     set_default_scheduler(4);
     for(int i = 0; i < num_actors; i++) spawn(veta_actor, file_arg);
     await_all_others_done();
