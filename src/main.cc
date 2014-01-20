32558f1272dc33fce23451be3d91b3b7af786f3e
Nik Voss <niklas.voss@gmail.com>
2014-01-20 18:47:46 +0000

    Untidy test fix for multi-threading.

--- src/main.cc
+++ src/main.cc
@@ -11,123 +11,69 @@
 #include <v8.h>
 #include <cppa/cppa.hpp>
 #include <cppa/scheduler.hpp>
+#include <thread>
 
 using namespace v8;
 using namespace std;
 using namespace cppa;
 
-map<string, Extension*> extension_map;
-
 void log(const FunctionCallbackInfo<Value> &args) {
     bool first = true;
     for (int i = 0; i < args.Length(); i++) {
         HandleScope handle_scope(args.GetIsolate());
-        if (first) {
+        if (first) 
             first = false;
-        } else {
+        else 
             aout << " ";
-        }
         String::Utf8Value utf8(args[i]);
         aout << *utf8;
     }
     aout << endl;
 }
 
-Handle<Context> create_veta_context(Isolate* isolate, const string &file) {
-    Handle<ObjectTemplate> global = ObjectTemplate::New();
-    global->Set(String::New("log"), FunctionTemplate::New(log));
-    const char* extensions[] = { file.c_str() };
-    return Context::New(isolate, new ExtensionConfiguration(1, extensions), global);
-}
+string file_source =  "log(\"Hello, world\");";
 
-int preload_file(string file) { // , bool base_checked = false) {
-    if (file.substr(file.size() - 3) != ".js") {
-        file += ".js";
-    }
-    if (extension_map.find(file) == extension_map.end()) {
-        ifstream file_stream(file.c_str());
-        if (!file_stream) {
-            // if (!base_checked) {
-            //     file_stream = check_other_includes(file); 
-            //     if (!file_stream)
-            //         return -2;
-            //
-            // } else {
-            cout << "Error: Unable to open file: " << file << endl;
-            return -1;
-            // }           
-        }
-        string* file_source = new string(); // make sure source isn't deleted.
-        file_stream.seekg(0, ios::end);   
-        file_source->reserve(file_stream.tellg());
-        file_stream.seekg(0, ios::beg);
-        file_source->assign((istreambuf_iterator<char>(file_stream)), istreambuf_iterator<char>());
-        // int include_error = check_source_includes(file_source);
-        // if (include_error != 0)
-        //     return include_error;
-        if (extension_map.find(file) == extension_map.end()) {
-            Extension* extension = new Extension(file.c_str(), file_source->c_str(), 0, 0, file_source->size());
-            extension_map[file] = extension; // NOTE: because of reference in map, string will persist
-            RegisterExtension(extension);
-        } else {
-            delete file_source;
-        } 
-    }
-    return 0;
-}
+__thread Isolate* isolate;
+__thread map<string, Persistent<Script>*>* script_map; 
+__thread Persistent<Context>* context;
 
-void veta_actor(const string &file) {
-    auto isolate = Isolate::New();
+void test_actor(const string &file) {
+    bool thread_initialized = (isolate != NULL);
+    if (!thread_initialized) {
+        isolate = Isolate::New();
+        script_map = new map<string, Persistent<Script>*>();
+    }
+    Locker locker(isolate);
+    Isolate::Scope isolate_scope(isolate);        
+    HandleScope handle_scope(isolate);
+    if (!thread_initialized) {
+        context = new Persistent<Context>();
+        auto global = ObjectTemplate::New();
+        global->Set(String::New("log"), FunctionTemplate::New(log));
+        Handle<Context> context_ = Context::New(isolate, NULL, global);
+        context->Reset(isolate, context_);
+    }
+    Handle<Context> local_context = Local<Context>::New(isolate, *context);
     {
-        Locker locker(isolate);
-        Isolate::Scope isolate_scope(isolate);        
-        HandleScope handle_scope(isolate);
-        Handle<Context> context = create_veta_context(isolate, file);
-        {
-            Context::Scope context_scope(context); 
-            auto script = Script::Compile(String::NewFromUtf8(isolate, ""));
-            Handle<Value> result = script->Run();
-            if (!result->IsUndefined()) {
-                String::Utf8Value utf8(result);
-                aout << *utf8 << endl;
-            }
+        Context::Scope context_scope(local_context); 
+        if (!thread_initialized) {
+            (*script_map)[file] = new Persistent<Script>();
+            (*script_map)[file]->Reset(isolate, Script::New(String::New(file_source.c_str(), file_source.size()), String::New(file.c_str(), file.size())));
+        }
+        Handle<Script> script = Local<Script>::New(isolate, *(*script_map)[file]);
+        Handle<Value> result = script->Run();
+        if (!result->IsUndefined()) {
+            String::Utf8Value utf8(result);
+            aout << *utf8 << endl;
         }
     }
-    isolate->Dispose();
-}
-
-int argv_to_int(char* arg) {
-    stringstream ss;
-    ss << arg;
-    int result;
-    ss >> result;
-    return result;
+    //isolate->Dispose();
 }
 
 int main(int argc, char* argv[]) {
-    std::string file_arg = "";
-    int num_actors = 1;
-    if (argc > 1) for (int i = 1; i < argc; i++) { 
-        if (strcmp(argv[i], "-n") == 0 && (i+1) < argc) {
-            i += 1;
-            num_actors = argv_to_int(argv[i]);            
-        } else {
-            string temp(argv[i]);
-            if (temp.size() > 3 && temp.substr(temp.size() - 3) == ".js") {
-                file_arg = temp;
-            }
-        }
-    }
-    if (file_arg == "") {
-        return 0;
-    }
-   
-    if (preload_file(file_arg) != 0) {
-        return 0;
-    }
- 
+    string file = "test.js";
     set_default_scheduler(4);
-    for(int i = 0; i < num_actors; i++) spawn(veta_actor, file_arg);
+    for(int i = 0; i < 1000; i++) spawn(test_actor, file);
     await_all_others_done();
     shutdown(); 
 
